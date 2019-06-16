a4bad744a6eb46612c0f9dd289c216446455d3d9
Emanuele Fabbiani <donlelef@gmail.com>
2019-06-16 20:45:50 +0200

    usr: new: create python package and include licence

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='jupyter-template',
+    version='0.5.0',
+    packages=[''],
+    url='',
+    license='MIT licence',
+    author='Emanuele Fabbiani',
+    author_email='donlelef@gmail.com',
+    description='Jupyter extension to support templates',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
