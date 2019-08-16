234327cb0759acb174f810da6257a47dd4852ad2
Emanuele Fabbiani <donlelef@gmail.com>
2019-08-16 17:13:24 +0200

    Update tests with shell option

--- tests/test_install.py
+++ tests/test_install.py
@@ -4,20 +4,25 @@ import pytest
 
 
 @pytest.mark.run(order=1)
-def test_can_install():
-    subprocess.run('jupyter nbextension install --py jupytemplate --sys-prefix').check_returncode()
+def test_can_install_package():
+    subprocess.run('python setup.py install', shell=True).check_returncode()
 
 
 @pytest.mark.run(order=2)
-def test_can_enable():
-    subprocess.run('jupyter nbextension enable jupytemplate/main --sys-prefix').check_returncode()
+def test_can_install():
+    subprocess.run('jupyter nbextension install --py jupytemplate --sys-prefix', shell=True).check_returncode()
 
 
 @pytest.mark.run(order=3)
-def test_can_disable():
-    subprocess.run('jupyter nbextension disable jupytemplate/main --sys-prefix').check_returncode()
+def test_can_enable():
+    subprocess.run('jupyter nbextension enable jupytemplate/main --sys-prefix', shell=True).check_returncode()
 
 
 @pytest.mark.run(order=4)
+def test_can_disable():
+    subprocess.run('jupyter nbextension disable jupytemplate/main --sys-prefix', shell=True).check_returncode()
+
+
+@pytest.mark.run(order=5)
 def test_can_uninstall():
-    subprocess.run('jupyter nbextension uninstall --py jupytemplate --sys-prefix').check_returncode()
+    subprocess.run('jupyter nbextension uninstall --py jupytemplate --sys-prefix', shell=True).check_returncode()
